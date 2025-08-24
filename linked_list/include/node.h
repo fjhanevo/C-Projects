13c3efd82c326708af0385174201615fe4aa3af9
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-24 19:17:21 +0200

    added insert_at_beginning, insert_at_end and free_list

--- linked_list/include/node.h
+++ linked_list/include/node.h
@@ -3,5 +3,8 @@ struct Node {
     struct Node *next;
 };
 
-struct Node* create_node(int n);
+struct Node* create_node(int data);
 void print_list(struct Node* head);
+void insert_at_beginning(struct Node** head_ref, int data);
+void insert_at_end(struct Node** head_ref, int data);
+void free_list(struct Node* head);
--- linked_list/src/node.c
+++ linked_list/src/node.c
@@ -23,5 +23,41 @@ void print_list(struct Node* head)
         printf("%d\n", current->data);
         current = current->next;
     }
-    printf("NULL");
+    printf("NULL\n");
+}
+
+void insert_at_beginning(struct Node** head_ref, int data)
+{
+    struct Node* node = create_node(data);
+    node->next = *head_ref;
+    *head_ref = node;
+}
+
+void insert_at_end(struct Node** head_ref, int data)
+{
+    struct Node* node = create_node(data);
+
+    // checks for empty list
+    if (*head_ref == NULL) {
+        *head_ref = node;
+        return;
+    }    
+
+    // navigate to last node, assign temp to the last node
+    struct Node* temp = *head_ref;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = node;
+}
+
+void free_list(struct Node* head)
+{
+    struct Node* current = head; 
+    while (current != NULL) {
+        struct Node* next_node = current->next;
+        free(current);
+        current = next_node;
+    }
+    head = NULL;
 }

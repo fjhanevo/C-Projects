f17bc928a33e5e772209866097a23a9a7471de96
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-24 22:29:35 +0200

    testing new functions

--- linked_list/src/main.c
+++ linked_list/src/main.c
@@ -1,24 +1,26 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 #include "node.h"
 
 int main(void) {
     struct Node* head = NULL;
 
+    print_list(head);
     insert_at_beginning(&head, 10);
     insert_at_beginning(&head, 20);
     insert_at_beginning(&head, 30);
 
-    printf("Initial list: \n");
+    print_list(head);
+    insert_at_position(&head, 66, 1);
+    insert_at_position(&head, 99, 100); // should be placed at the end, as this position is invalid
+    print_list(head);
+    delete_at_position(&head, 1);
+    delete_at_position(&head, 200); // should not delete anything as the position is invalid
+    print_list(head);
+    free_list(&head);
     print_list(head);
 
-    /* insert_at_beginning(&head, 15); */
-    /* insert_at_end(&head, 155); */
-    // should be val 69 at position 2
-    insert_at_position(&head, 69, 1); 
-
-    printf("Modified list: \n");
-    print_list(head);
+    
         
     return 0;
 }

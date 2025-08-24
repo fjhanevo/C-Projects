d0ccfe9d6b95f4fede2d03b2d4e8f5fd91e2315e
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-24 19:17:53 +0200

    added a test to main

--- linked_list/src/main.c
+++ linked_list/src/main.c
@@ -4,5 +4,20 @@
 
 int main(void) {
     struct Node* head = NULL;
+
+    insert_at_beginning(&head, 10);
+    insert_at_beginning(&head, 20);
+    insert_at_beginning(&head, 30);
+
+    printf("Initial list: \n");
+    print_list(head);
+
+    insert_at_beginning(&head, 15);
+    insert_at_end(&head, 155);
+
+    printf("Modified list: \n");
+    print_list(head);
+        
     return 0;
 }
+

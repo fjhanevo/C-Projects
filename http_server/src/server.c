18835ac56fde980140c219fde363e71698eefaf8
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-29 16:36:48 +0200

    changed PORT to port in the htons() call, started implementing run_server

--- http_server/src/server.c
+++ http_server/src/server.c
@@ -12,7 +12,7 @@ int listening_socket(int port)
     struct sockaddr_in addr;
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("listening socket failed!\n");
+        perror("listening socket failed!");
         return -1;
     }
 
@@ -25,7 +25,7 @@ int listening_socket(int port)
     }
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
+    addr.sin_port = htons(port);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     // bind the socket
@@ -43,3 +43,16 @@ int listening_socket(int port)
     return server_fd;
 }
 
+void run_server(int server_fd)
+{
+    int client_fd;    
+    struct sockaddr_in addr;
+    socklen_t addrlen = sizeof(addr); 
+
+    while (1) {
+        if ((client_fd= accept(server_fd, (struct sockaddr*)&addr, &addrlen)) < 0) {
+            perror("accept");
+            return;
+        }
+    }
+}

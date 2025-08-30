a6543cf0648413bf5dc274c86141b775a8dd9b90
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-30 11:29:10 +0200

    added header guards

--- http_server/include/http.h
+++ http_server/include/http.h
@@ -1,4 +1,12 @@
+#ifndef HTTP_H
+#define HTTP_H
+
+#include <stdio.h>
+
 void handle_http_request(int client_fd);
 int parse_request(const char *request, char *method, char *path);
 void send_http_response(int client_fd, const char *path);
 void send_404_response(int client_fd);
+void send_200_response(int client_fd, FILE* file, const char *file_path);
+
+#endif // HTTP_H
--- http_server/include/server.h
+++ http_server/include/server.h
@@ -1,2 +1,7 @@
+#ifndef SERVER_H
+#define SERVER_H
+
 int listening_socket(int port);
 void run_server(int server_fd);
+
+#endif // SERVER_H 

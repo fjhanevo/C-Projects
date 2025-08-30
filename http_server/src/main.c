bec4ad1e9c26ed7d6ab36d617d61e1bc871c20ec
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-30 12:27:18 +0200

    implemented a proper main function

--- http_server/src/main.c
+++ http_server/src/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "server.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("fuck\n");
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+    int server_fd = listening_socket(port);
+    if (server_fd < 0) {
+        fprintf(stderr, "Failed to create listening socket\n");
+        return 1;
+    }
+
+    printf("Server listening on port: %d\n", port);
+    run_server(server_fd);
+
     return 0;
 }
 

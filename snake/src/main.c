87dcbd7623a5336814beb29a5f15d91327f432ae
fjhanevo <fredrik.hanevold@gmail.com>
2025-09-05 23:05:43 +0200

    added in snake.h

--- snake/src/main.c
+++ snake/src/main.c
@@ -1,9 +1,10 @@
 #include <ncurses.h>
+#include "snake.h"
 
 int main(void)
 {
     initscr();                  // initialize
-    printw("Hello world");      // print hello world
+    snake_test();               // print something
     refresh();                  // print it on the real screen
     getch();                    // wait for user input
     endwin();                   // end 

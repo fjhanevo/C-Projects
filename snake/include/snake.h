7f3f142a5a6f3981c19cd11b9f09f970b2621240
fjhanevo <fredrik.hanevold@gmail.com>
2025-09-07 12:22:39 +0200

    declared new structs

--- snake/include/snake.h
+++ snake/include/snake.h
@@ -1,5 +1,31 @@
 #ifndef SNAKE_H
 #define SNAKE_H
+#include <time.h>
+
+#define SNAKE_SIZE 100
+
+typedef struct {
+    int x;
+    int y;
+} Point; 
+
+typedef struct {
+    Point pos;
+    time_t spawn_time;
+} Food;
+
+typedef struct {
+    Point pos[SNAKE_SIZE];
+    int length;
+} Snake;
+
+typedef struct {
+    Snake snake;
+    Food food;
+    int score;
+    int height;
+    int width;
+} GameState;
 
 void play_snake();
 
